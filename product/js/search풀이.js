19ef9ede8c01eec487bacdb6416e1cbd5e1905e0
leeseongchi <kyoti0414@naver.com>
2024-02-15 17:41:53 +0900

    240215 Product 풀이

---
+++ product/js/search풀이.js
@@ -0,0 +1,30 @@
+async function handleSearchClick1() {
+    try {
+        const response = await fetch("http://localhost:8080/product/products");
+
+        const responseData = await response.json();
+
+        const productList = document.querySelector(".product-list");
+        productList.innerHTML = "";
+
+        
+        for(let product of responseData) {
+            productList.innerHTML += ProductInfoTr(product);
+        }
+
+
+    } catch(error) {
+        console.log(error);
+}
+
+function ProductInfoTr({productId, productName, productPrice, productSize}) {
+    return `
+        <tr>
+            <td> ${productId} </td>
+            <td> ${productName} </td>
+            <td> ${productPrice} </td>
+            <td> ${productSize} </td>
+        </tr>
+    `;
+    }
+}
